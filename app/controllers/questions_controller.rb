4f19a70d198abbe5bf3395a279a98e73efcb8987
Nikita Ahuja <email": null>
2017-10-30 14:55:39 -0500

    Add edit functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
   end
 
   def show
@@ -8,14 +9,32 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   def create
-    @question = Question.new(question_params)
+  @question = Question.new(question_params)
 
-    @question.save
+  if @question.save
     redirect_to @question
+  else
+    render 'new'
   end
+end
+
+def update
+  @question = Question.find(params[:id])
+
+  if @question.update(question_params)
+    redirect_to @question
+  else
+    render 'edit'
+  end
+end
 
 private
   def question_params
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class  Question < ApplicationRecord
+  validates :question_title, presence: true,
+                    length: { minimum: 5 }
+
 end
